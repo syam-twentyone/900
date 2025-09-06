42e84eba406bbf99f1964696cb395ef2ada3bc70
syam-twentyone <twenty.one210794@gmail.com>
2025-09-06 12:51:48 +0800

    Update utils.mjs

--- module/utils.mjs
+++ module/utils.mjs
@@ -1,4 +1,4 @@
-/* Copyright (C) 2023-2025 anonymous
+/* Copyright (C) 2023-2024 anonymous
 
 This file is part of PSFree.
 
@@ -29,12 +29,23 @@ export function die(msg='') {
 }
 
 const console = document.getElementById('console');
-export function log(msg='') {
+export function debug_log(msg='') {
     console.append(msg + '\n');
 }
 
 export function clear_log() {
     console.innerHTML = null;
+}
+
+export function str2array(str, length, offset) {
+    if (offset === undefined) {
+        offset = 0;
+    }
+    let a = new Array(length);
+    for (let i = 0; i < length; i++) {
+        a[i] = str.charCodeAt(i + offset);
+    }
+    return a;
 }
 
 // alignment must be 32 bits and is a power of 2
@@ -44,8 +55,8 @@ export function align(a, alignment) {
     }
     const mask = -alignment & 0xffffffff;
     let type = a.constructor;
-    let low = a.lo & mask;
-    return new type(low, a.hi);
+    let low = a.low & mask;
+    return new type(low, a.high);
 }
 
 export async function send(url, buffer, file_name, onload=() => {}) {
@@ -57,7 +68,7 @@ export async function send(url, buffer, file_name, onload=() => {}) {
     const form = new FormData();
     form.append('upload', file);
 
-    log('send');
+    debug_log('send');
     const response = await fetch(url, {method: 'POST', body: form});
 
     if (!response.ok) {
@@ -81,92 +92,4 @@ export function hex(number) {
 // no "0x" prefix
 export function hex_np(number) {
     return number.toString(16);
-}
-
-// expects a byte array
-export function hexdump(view) {
-    const num_16 = view.length & ~15;
-    const residue = view.length - num_16;
-    const max_off_len = hex_np(((view.length + 7) & ~7) - 1).length;
-
-    function chr(i) {
-        if (0x20 <= i && i <= 0x7e) {
-            return String.fromCodePoint(i);
-        }
-        return '.';
-    }
-
-    function to_hex(view, offset, length) {
-        return (
-            [...view.slice(offset, offset + length)]
-            .map(e => hex_np(e).padStart(2, '0'))
-            .join(' ')
-        );
-    }
-
-    let bytes = [];
-    for (let i = 0; i < num_16; i += 16) {
-        const long1 = to_hex(view, i, 8);
-        const long2 = to_hex(view, i + 8, 8);
-
-        let print = '';
-        for (let j = 0; j < 16; j++) {
-            print += chr(view[j]);
-        }
-
-        bytes.push([`${long1}  ${long2}`, print]);
-    }
-
-    if (residue) {
-        const small = residue <= 8;
-        const long1_len = small ? residue : 8;
-
-        let long1 = to_hex(view, num_16, long1_len);
-        if (small) {
-            for (let i = 0; i < 8 - residue; i++) {
-                long1 += ' xx';
-            }
-        }
-
-        const long2 = (() => {
-            if (small) {
-                return Array(8).fill('xx').join(' ');
-            }
-
-            let res = to_hex(view, num_16 + 8, residue - 8);
-            for (let i = 0; i < 16 - residue; i++) {
-                res += ' xx';
-            }
-
-            return res;
-        })();
-
-        let print = '';
-        for (let i = 0; i < residue; i++) {
-            print += chr(view[num_16 + i]);
-        }
-        for (let i = 0; i < 16 - residue; i++) {
-            print += ' ';
-        }
-
-        bytes.push([`${long1}  ${long2}`, print]);
-    }
-
-    for (const [pos, [val, print]] of bytes.entries()) {
-        const off = hex_np(pos * 16).padStart(max_off_len, '0');
-        log(`${off} | ${val} |${print}|`);
-    }
-}
-
-// make a JavaScript string
-export function jstr(buffer) {
-    let res = '';
-    for (const item of buffer) {
-        if (item === 0) {
-            break;
-        }
-        res += String.fromCodePoint(item);
-    }
-    // convert to primitive string
-    return String(res);
 }
